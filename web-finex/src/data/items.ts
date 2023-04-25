b810d821ad2ea329f993a8c96c7b3a51559b6f36
Gabriel <gabrielnino@edu.unifil.br>
2023-04-25 18:01:30 -0300

    Primeira versão da lista de despesas

---
+++ web-finex/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+	{ date: new Date(2023, 3, 6), category: 'food', title: 'McDonalds', value: 32.12 },
+	{ date: new Date(2023, 4, 15), category: 'food', title: 'Burger King', value: 28.00 },
+	{ date: new Date(2023, 5, 16), category: 'rent', title: 'Aluguel Apt', value: 2300.00 },
+	{ date: new Date(2023, 2, 18), category: 'salary', title: 'Salário ACME', value: 4500.00 },
+];
